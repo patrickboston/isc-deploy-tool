be4ebe561002bddd28aaf2af046061f4b43ebd02
Patrick Boston <patrickboston8@gmail.com>
2023-11-10 11:33:27 -0500

    Updating gitignore to include target.js and reverse.target.js

--- reverse.target.js
+++ reverse.target.js
@@ -1,5 +1,10 @@
 export default
     {
+        "SOURCE/Active Directory - Test Import2.json": {
+            "$.object.owner.id": "%%AD_OWNER_ID%%",
+            "$.object.connectorAttributes.IQServicePort": "%%AD_IQSERVICE_PORT%%",
+            "$.notfound": "%%ABC%%"
+        },
         "SOURCE/Active Directory.json": {
             "$.object.owner.id": "%%AD_OWNER_ID%%",
             "$.object.connectorAttributes.IQServicePort": "%%AD_IQSERVICE_PORT%%",
