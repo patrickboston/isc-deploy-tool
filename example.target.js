c5ffd378ff343eb661cf7f8b477827254f0bc2c2
Patrick Boston <patrickboston8@gmail.com>
2024-07-11 16:06:29 -0400

    ICI-103 cleanup samples

--- example.target.js
+++ example.target.js
@@ -1,5 +1,5 @@
 export default
     {
-        "%%AD_OWNER_ID%%": "ABCD1234",
-        "%%AD_IQSERVICE_PORT%%": "888888",
+        "%%AD_OWNER_ALIAS%%": "ABCD1234",
+        "%%AD_IQSERVICE_PORT%%": "5052",
     }
--- reverse.target.js
+++ reverse.target.js
@@ -2,11 +2,6 @@ export default
     {
         "SOURCE/Active Directory/Active Directory.json": {
             "$.owner.name": "%%AD_OWNER_ALIAS%%",
-            "$.connectorAttributes.IQServicePort": "%%AD_IQSERVICE_PORT%%",
-            "$.notfound": "%%ABC%%"
-        },
-        "SOURCE/JAR TEST/JAR TEST.json": {
-        },
-        "ACCESS_REQUEST_CONFIG/ACCESS_REQUEST_CONFIG.json": {
+            "$.connectorAttributes.IQServicePort": "%%AD_IQSERVICE_PORT%%"
         }
     }
